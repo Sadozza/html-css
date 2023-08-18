2aee267914035a7016d62db4f586e97ea3ea079b
Cara <sawadalukaz@gmail.com>
2023-08-18 11:04:27 -0300

    2323
    121

---
+++ script.js
@@ -0,0 +1,14 @@
+// Código JavaScript para interações na página
+const addToCartButtons = document.querySelectorAll(".add-to-cart");
+
+addToCartButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        alert("Produto adicionado ao carrinho!");
+    });
+});
+Lembre-se de substituir os nomes de arquivos de imagem, como "product1.jpg" e "product2.jpg", pelos nomes reais dos seus produtos. Além disso, este é apenas um exemplo básico e pode ser expandido com recursos mais avançados, como páginas de detalhes do produto, um carrinho de compras funcional, etc.
+
+
+
+
+
